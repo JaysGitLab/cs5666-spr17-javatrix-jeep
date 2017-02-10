4fdf7a0dff3f8c5b74547be58f3269addd99eecf
erick lares <erick_lares@hotmail.com>
2017-02-10 15:44:17 -0500

    Added comments, style and check style.

--- Matrix.java
+++ Matrix.java
@@ -1,21 +1,49 @@
+/**
+ * Matrix.java
+ *
+ * @author Erick Lares
+ * @author Eric Cambel
+ * @author Praveen Mariappan
+ * @version Feb 2017
+ */
+
+/**
+*The Matrix class provides 
+*implementation for all the methods 
+*in Javatrix Java Matrix Class. 
+*
+*@author Erick Lares
+*@author Eric Cambel
+*@author Praveen Mariappan
+*
+*@version Feb 2017
+ */
+
 public class Matrix extends java.lang.Object 
-    implements java.lang.Cloneable, java.io.Serializable {
+    implements java.lang.Cloneable, java.io.Serializable 
+{
    
-	static double [][] matrix;
-	static int m;
-	static int n;
-	public  Matrix(int m, int n) {
-   	
-		this.m = m;
-		this.n = n;
-		matrix = new double[m][n];
-	
-		for (int i = 0; i < m; i++){
-	
-			for (int j = 0; j < n; j++)
-			{
-				matrix [i][j] = 0;	
-			}	
-		}
+    static double [][] matrix;
+    static int m;
+    static int n;
+/**
+*
+*Constructor that build a matrix full of zeros.
+*
+*@param m rows
+*@param n rows
+*/
+    public  Matrix(int m, int n) 
+    {
+        this.m = m;
+	this.n = n;
+	matrix = new double[m][n];
+	for (int i = 0; i < m; i++)
+	{
+	    for (int j = 0; j < n; j++)
+	    {
+	        matrix [i][j] = 0;	
+	    }	
 	}
+    }
 }
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
 JUNIT_JAR = /usr/share/java/junit-4.10.jar
 HAMCREST_JAR = /usr/share/java/hamcrest/core-1.1.jar
+CKSTYLE_COMMAND =  -jar /usr/local/checkstyle-5.5/checkstyle-5.5-all.jar
 
 default: 
 	@echo "usage: make target"
@@ -16,3 +17,6 @@ clean:
 
 test: Matrix.class
 	java -cp .:$(JUNIT_JAR):$(HAMCREST_JAR) org.junit.runner.JUnitCore MatrixTest
+
+check: Matrix.java MatrixTest.java
+	checkstyle Matrix.java MatrixTest.java
